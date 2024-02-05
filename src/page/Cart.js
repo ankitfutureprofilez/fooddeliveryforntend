d4b3111af6b21b55ed58a459fb76b95d021c3b67
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-02-05 16:42:28 +0530

    token

--- src/page/Cart.js
+++ src/page/Cart.js
@@ -29,7 +29,7 @@ const Cart = () => {
 
       try {
         const res = await fetch(
-          "http://localhost:8000/create-checkout-session",
+          `${process.env.REACT_APP_BASE_URL}/create-checkout-session`,
           {
             method: "POST",
             headers: {
