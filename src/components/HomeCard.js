e694fa78bf0e4b78c11f66aec8c9b093c754865c
Abhinav-FP <a.mathur@futureprofilez.com>
2024-02-05 11:06:58 +0530

    First Com

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 const HomeCard = ({ name, image, category, price, loading,id }) => {
   return (
     <div className="bg-white shadow-md p-2 rounded min-w-[150px]">
