f198d1362d43aea0b21bdf5730abedfd127fe36f
Abhinav-FP <a.mathur@futureprofilez.com>
2024-02-07 12:55:37 +0530

    Home page cart made dynamic

--- src/page/Home.js
+++ src/page/Home.js
@@ -24,24 +24,29 @@ const Home = () => {
   const preveProduct = () => {
     slideProductRef.current.scrollLeft -= 200;
   };
-
+  const cartItemNumber = useSelector((state)=>state.product.cartItem)
 
   return (
-
     <div className="flex">
-      <div className="w-2/3">
-        <div className="bg-white p-2 md:p-4 pt-6 md:pt-10 ">
-          <AllProduct heading={"Your Product"} />
+      {cartItemNumber.length > 0 ? (
+        <>
+          <div className="w-2/3">
+            <div className="bg-white p-2 md:p-4 pt-6 md:pt-10 ">
+              <AllProduct heading={"Your Product"} />
+            </div>
+          </div>
+          <div className="w-1/3">
+            <Carthome/>
+          </div>
+        </>
+      ) : (
+        <div className="w-full">
+          <div className="bg-white p-2 md:p-4 pt-6 md:pt-10 ">
+            <AllProduct heading={"Your Product"} />
+          </div>
         </div>
-      </div>
-      <div className="w-1/3">
-   <Carthome/>
-      </div>
+      )}
     </div>
-    // <div className="bg-white p-2 md:p-4 pt-6 md:pt-10 ">     
-    //   <AllProduct heading={"Your Product"}/>
-    // </div>
-
   );
 };
 
