8602727bc6611b9a1abed81240dae873b39bbdcd
unknown <ankit.jain0748@gmail.com>
2024-02-12 20:47:08 +0530

    changement

--- src/page/RestaurantInfo.js
+++ src/page/RestaurantInfo.js
@@ -2,53 +2,59 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from 'react-router-dom';
 import { LuMapPin } from "react-icons/lu";
 import Listings from "../Api/Listings";
+import { FaUserGroup } from "react-icons/fa6";
+import { FaUserAlt } from "react-icons/fa";
+import { IoTime } from "react-icons/io5";
 
 export default function RestaurantInfo() {
+
   const [record, setRecord] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [loading ,setLoading] =useState(true)
+  
   const fetchData = async () => {
     try {
-      const main = new Listings();
-      const response = await main.resturantget();
-      setRecord(response.data.list);
-      setLoading(false);
+        const main = new Listings();
+        const response = await main.resturantget();
+        setRecord(response.data.list);
+        setLoading(false);
     } catch (error) {
-      console.log("error", error);
-      setLoading(false);
+        console.log("error", error);
+        setLoading(false);
     }
   };
-
+  
   useEffect(() => {
     fetchData();
-  }, []); console.log("ddd", record)
-
+  }, []); console.log("ddd",record)
   return (
     <>
       <div className="bg-white p-2 md:p-4 pt-6 md:pt-10">
         <div className="flex flex-wrap pt-10 -mx-3">
           {record && record?.map((item, index) => (
-            <Link key={index} to={`/restaurants/${item.resId}`} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/6 px-3 mb-6">
+            <Link key={index} to={`/restaurants/${item.resId}`} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/5 px-3 mb-6">
               <div className="w-full bg-white product_box py-3 px-3 cursor-pointer flex flex-col rounded-xl">
                 <div className="flex flex-col justify-center items-center">
-                  <img alt="image" src={item.image} className="rounded-xl w-full h-44 object-cover" />
+                  <img alt="image" src={item.banner_image} className="rounded-xl w-full h-44 object-cover" />
                 </div>
                 <h3 className="font-bold text-lg text-gray-900 capitalize text-base mt-2 mb-1 whitespace-nowrap overflow-hidden">
                   {item.restaurantname}
                 </h3>
-                <p className="text-gray-600">{item.category}</p>
-                <div className="flex justify-between mt-1">
-                  <div>
-                    <p className="text-gray-400 text-sm align-middle mb-1 relative pl-4">
-                      <span><LuMapPin className="inline text-gray-400 absolute -left-1 top-1" size={16} /> {item.description}</span>
-                      <span> {item.location}</span>
-                      <span> {item.staff}</span>
-                      <span> {item.ownername}</span>
+                <p className="text-gray-600 mb-2">{item.category}</p>
+                <p className="text-gray-500 text-sm align-middle mb-1 relative ">
+                  <span> {item.description}</span >
+                </p>
+                <div className="mt-1">
+                    <p className="text-gray-400 text-sm align-middle mb-1 relative pl-5">
+                      <span><LuMapPin className="inline text-gray-400 absolute left-0 top-0.5" size={16} /> {item.location}</span></p>
+                    <p className="text-gray-400 whitespace-nowrap  text-sm align-middle mb-1 relative pl-5">  <span> <FaUserAlt className="inline text-gray-400 absolute left-0 top-0.5" size={13} /> {item.ownername}</span> </p>
 
-                      <span> {item.
-                        opening_from} - {item.opening_to}</span>
-                    </p>
-                    <p className="text-green-500">Free Delivery</p>
-                  </div>
+                    <div className="flex flex-warp space-x-2 mb-1">
+
+                      <p className="text-gray-400 text-sm align-middle mb-1 relative pl-5"><span> <FaUserGroup className="inline text-gray-400 absolute left-0 top-0.5" size={16} /> {item.staff}</span>  </p>                   
+                      <p className="text-gray-400 text-sm align-middle mb-1 relative pl-5">  <span> <IoTime  className="inline text-gray-400 absolute left-0 top-0.5" size={16} /> {item.opening_from} - {item.opening_to}</span>
+                      </p>
+                    </div>
+                     <p className="text-green-500">Free Delivery</p>
                 </div>
               </div>
             </Link>
@@ -56,6 +62,5 @@ export default function RestaurantInfo() {
         </div>
       </div>
     </>
-  );
+  )
 }
-
--- src/page/Restaurantdetails.js
+++ src/page/Restaurantdetails.js
@@ -1,55 +1,54 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import Listings from '../Api/Listings';
-import UserProduct from './UserProduct';
-
+import UserProduct from  "./UserProduct"
 export default function Restaurantdetails() {
   const { resId } = useParams();
   console.log("resId", resId)
   const [record, setRecord] = useState([])
-  const [loading, setLoading] = useState(true);
+
+const[loading ,setLoading] =useState(true);
+
   const fetchData = async () => {
     try {
-      const main = new Listings();
-      const response = await main.resturantdetilas(resId);
-      setRecord(response.data.record[0]);
-      setLoading(false);
+        const main = new Listings();
+        const response = await main.resturantdetilas(resId);
+        setRecord(response.data.record[0]);
+        setLoading(false);
     } catch (error) {
-      console.log("error", error);
-      setLoading(false);
+        console.log("error", error);
+        setLoading(false);
     }
   };
+  
   useEffect(() => {
     fetchData();
-  }, []);
-  
-  const userId = record.userId;
+  }, []); 
+
+  const userId =record.userId;
   return (
     <>
-      <div class="flex flex-row ">
-        <div>
-          <img src={record.image} alt={record.index} />
-        </div>
-        <div>
-          <div class="flex flex-nowrap">
-            <div>
-              <h1>{record.restaurantname}</h1>
-              <p>{record.category
-              }</p>
-              <p>{record.description}</p>
-              <p>{record.location}</p>
-              <p>Timing :- {record.
-                opening_to
-              } {record.
-                opening_from} </p>
+      <div className='bg-white p-2 md:p-4 pt-6 md:pt-10'>
+        <div class="flex flex-row ">
+          <div className='w-full md:w-6/12 '>
+            <div className=' bg-white product_box  py-3 px-3 cursor-pointer'>
+               <img src={record.banner_image} alt={record.index} />
             </div>
-
           </div>
-
+          <div className='w-full md:w-6/12 '>
+            <div class="flex flex-nowrap">
+              <div>
+                <h1>{record.restaurantname}</h1>
+                <p>{record.category}</p>
+                <p>{record.description}</p>
+                <p>{record.location}</p>
+                <p>Timing :- {record.opening_to} {record.opening_from} </p>
+              </div>
+            </div>
+          </div>
         </div>
-
       </div>
-      <UserProduct  userId = {userId}/>
+      <UserProduct userId ={userId}/>
     </>
   )
 }
