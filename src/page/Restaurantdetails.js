b62ca604d64430c518a5683793174e266e310593
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-02-13 11:47:27 +0530

    chnangement:

--- src/page/Restaurantdetails.js
+++ src/page/Restaurantdetails.js
@@ -26,28 +26,25 @@ export default function Restaurantdetails() {
   const userId = record.userId;
   return (
     <>
-      <div class="flex flex-row ">
-        <div>
-          <img src={record.banner_image} alt={record.index} />
-        </div>
-        <div>
-          <div class="flex flex-nowrap">
-            <div>
-              <h1>{record.restaurantname}</h1>
-              <p>{record.category
-              }</p>
-              <p>{record.description}</p>
-              <p>{record.location}</p>
-              <p>Timing :- {record.
-                opening_to
-              } {record.
-                opening_from} </p>
+         <div className='bg-white p-2 md:p-4 pt-6 md:pt-10'>
+        <div class="flex flex-row ">
+          <div className='w-full md:w-6/12 '>
+            <div className=' bg-white product_box  py-3 px-3 cursor-pointer'>
+               <img src={record.banner_image} alt={record.index} />
             </div>
-
           </div>
-
+          <div className='w-full md:w-6/12 '>
+            <div class="flex flex-nowrap">
+              <div>
+                <h1>{record.restaurantname}</h1>
+                <p>{record.category}</p>
+                <p>{record.description}</p>
+                <p>{record.location}</p>
+                <p>Timing :- {record.opening_to} {record.opening_from} </p>
+              </div>
+            </div>
+          </div>
         </div>
-
       </div>
       <UserProduct  userId = {userId}/>
     </>
