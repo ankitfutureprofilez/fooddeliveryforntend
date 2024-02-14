3ef9b0193d8e3e082950271a0247d566fb119685
Abhinav-FP <a.mathur@futureprofilez.com>
2024-02-14 18:36:51 +0530

    Setting opening

--- src/page/Restaurantdetails.js
+++ src/page/Restaurantdetails.js
@@ -17,16 +17,59 @@ export default function Restaurantdetails() {
       const response = await main.resturantdetilas(resId);
       setRecord(response.data.record[0]);
       setLoading(false);
+     
     } catch (error) {
       console.log("error", error);
       setLoading(false);
     }
   };
+  
+
+
   useEffect(() => {
     fetchData();
   }, []);
+  function convert12to24(timeString) {
+    if (timeString.length !== 6) {
+      return null; // Or handle the error appropriately
+    }
+    // Separate the time string into hours, minutes, and period
+    const hours = parseInt(timeString.substring(0, 2));
+    const period = timeString.substring(2).toUpperCase();
+
+    // Convert "12 AM" to "00" and "12 PM" to "12"
+    const hours24 =
+      period === "AM"
+        ? hours === 12
+          ? 0
+          : hours
+        : hours === 12
+        ? 12
+        : hours + 12;
+
+    // Return the time in 24-hour format
+    return hours24;
+  }
+
+  function isOpen(openingTime, closingTime) {
+    console.log("Opening Time",openingTime);
+    console.log("Closing Time",closingTime)
+    let date = new Date();
+    let currentHour = date.getHours();
+    let openingHour = convert12to24(openingTime);
+    let closingHour = convert12to24(closingTime);
+    if (openingHour === null || closingHour === null) {
+      return null;
+    }
+    if (openingHour <= currentHour && currentHour < closingHour) {
+      return true;
+    } else if (closingHour < openingHour && openingHour <= currentHour) {
+      return true;
+    } else return false;
+  }
 
   const userId = record.userId;
+
   return (
     <>
       <div className="flex flex-col md:flex-row mt-8">
@@ -74,13 +117,20 @@ export default function Restaurantdetails() {
             <div className="flex items-center mb-4">
               <FaRegClock size={15} className="mr-2" />
               <p className="text-sm text-gray-600 ">
-                Timing: {record.opening_to} - {record.opening_from}
+                Timing: {record.opening_from} - {record.opening_to}
               </p>
             </div>
             {/* </div> */}
             <p className="text-sm text-gray-700 mb-4 ml-6">
               {record.description}
-            </p>
+             </p>
+            {/* {isOpen(record.opening_from, record.opening_to) === true ? (
+              <p className="text-sm text-gray-700 mb-4 ml-6">open hai</p>
+            ) : isOpen(record.opening_from, record.opening_to) === null ? (
+              <p className="text-sm text-gray-700 mb-4 ml-6">Null</p>
+            ) : (
+              <p className="text-sm text-gray-700 mb-4 ml-6">Null</p>
+            )}  */}
           </div>
         </div>
       </div>
