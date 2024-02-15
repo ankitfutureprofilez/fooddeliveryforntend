a818bfd9b63525039fc70c7e89789845347d19f6
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2024-02-15 17:19:15 +0530

    latest changes added

--- src/page/Restaurantdetails.js
+++ src/page/Restaurantdetails.js
@@ -33,33 +33,64 @@ export default function Restaurantdetails() {
 
   return (
     <>
-      <div className='bg-white p-4 md:p-8 pt-6 md:pt-10'>
+
+
+      <div className='product-details bg-white p-4 md:p-8 pt-6 md:pt-10'>
         <div class="flex flex-row -mx-4">
           <div className='w-full md:w-5/12 px-4'>
             <div className=' bg-white product_box  py-3 px-3 cursor-pointer'>
                <img className='max-w-full min-h-[300px]' src={record && record.image} alt={record && record.index} />
             </div>
           </div>
-          <div className='w-full md:w-6/12 '>
-            <div class="flex flex-nowrap">
-              <div>
-                <h1>{record && record.restaurantname}</h1>
-                <p>{record && record.category}</p>
-                <p>{record && record.description}</p>
-                <p>{record && record.location}</p>
-                <p>Timing :- {record && record.opening_to} {record && record.opening_from} </p>
-              </div>
+          <div className="w-full md:w-7/12 mt-4 md:mt-0">
+          <div className="flex flex-col justify-center h-full ps-5">
+            <h1 className=" text-3xl font-semibold text-gray-800 mb-4">
+              {record && record.restaurantname}
+            </h1>
+            <div className="flex items-center mb-4">
+              {record && record.category === "veg" ? (
+                <>
+                  <FaRegDotCircle color="Green" size={15} className="mr-2" />
+                  <p className="text-sm text-gray-600">
+                    Vegetarian Food Available
+                  </p>
+                </>
+              ) : record && record.category === "nonveg" ? (
+                <>
+                  <FaRegDotCircle color="#ff0000" size={15} className="mr-2" />
+                  <p className="text-sm text-gray-600">
+                    Non Vegetarian Food Available
+                  </p>
+                </>
+              ) : record && record.category === "both" ? (
+                <p className="text-sm text-gray-600">
+                  Both veg and non-veg Food Available
+                </p>
+              ) : null}
+            </div>
+
+            {/* <div className="flex flex-wrap items-center mb-4"> */}
+            <div className="flex items-center mb-4 mr-4">
+              <p className="text-sm text-gray-600 flex"><FaLocationCrosshairs size={22} className="mr-2" /> {record && record.location}</p>
+            </div>
+            <div className="flex items-center mb-4">
+              <p className="text-sm text-gray-600 flex ">
+              <FaRegClock size={15} className="mr-2" /> Timing: {record && record.opening_from} - {record && record.opening_to}
+              </p>
             </div>
             {/* </div> */}
-            <p className="text-sm text-gray-700 mb-4 ml-6">
+            <p className="text-sm text-gray-700 mb-4 ">
               {record && record.description}
             </p>
-            {record && record.opening_from !== null && record && record.opening_to !== null ? (
-                <p className="text-sm text-gray-700 mb-4 ml-6">{isOpen(record && record.opening_from, record && record.opening_to)}</p>
-             ) : null} 
           </div>
+        </div>
         </div> 
       </div>
+
+
+     
+
+
 
       <UserProduct userId={userId} />
     </>
