851c0fa415f82ce31f1c01d034b1516731ff920e
unknown <ankit.jain0748@gmail.com>
2024-02-17 15:30:26 +0530

    ass

--- src/page/Restaurantdetails.js
+++ src/page/Restaurantdetails.js
@@ -1,54 +1,116 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
-import Listings from '../Api/Listings';
-import UserProduct from  "./UserProduct"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Listings from "../Api/Listings";
+import UserProduct from "./UserProduct";
+import { FaLocationCrosshairs, FaRegClock } from "react-icons/fa6";
+import { FaRegDotCircle } from "react-icons/fa";
+import useTimeCalculate from "../hooks/useTimeCalculate";
+import LoadingPage from "./LoadingPage";
+
 export default function Restaurantdetails() {
-  
-  const [record, setRecord] = useState([])
-
-const[loading ,setLoading] =useState(true);
-
+  const [record, setRecord] = useState([]);
+  const [loading, setLoading] = useState(true);
   const fetchData = async () => {
     try {
-        const main = new Listings();
-        const response = await main.resturantget();
-        console.log("response",response)
-        setRecord(response.data.record);
-        setLoading(false);
+      const main = new Listings();
+      const response = await main.resturantget();
+      console.log("rsponse", response);
+      setRecord(response.data.record);
+      setLoading(false);
     } catch (error) {
-        console.log("error", error);
-        setLoading(false);
+      console.log("error", error);
+      setLoading(false);
     }
   };
-  
+
   useEffect(() => {
     fetchData();
-  }, []); 
+  }, []);
 
-  const userId =record.userId;
+  const [isOpen] = useTimeCalculate();
+
+  const userId = record && record.userId;
+
   return (
     <>
-      <div className='bg-white p-2 md:p-4 pt-6 md:pt-10'>
-        <div class="flex flex-row ">
-          <div className='w-full md:w-6/12 '>
-            <div className=' bg-white product_box  py-3 px-3 cursor-pointer'>
-               <img src={record.image} alt={record.index} />
-            </div>
+      {loading ? (
+        <LoadingPage />
+      ) : (
+        <div>
+  {/* Image covering the entire width of the screen */}
+  <div className="w-full h-80 bg-cover bg-center">
+  <img
+    className="w-full h-80 "
+    src={record && record.image}
+    alt={record && record.index}
+  />
+</div>
+
+
+  {/* Existing component structure */}
+    <div className="flex flex-wrap justify-between product-details flex ">
+      <div className="w-full md:w-1/3 ">
+      <div className="flex flex-col justify-center h-full md:pt-4 pt-3 ">
+          <h1 className="text-3xl font-semibold text-gray-800 mb-4">
+            {record && record.restaurantname}
+          </h1>
+          <div className="flex items-center mb-4">
+            {record && record.category === "veg" ? (
+              <>
+                <FaRegDotCircle
+                  color="Green"
+                  size={15}
+                  className="mr-2"
+                />
+                <p className="text-sm text-gray-600">
+                  Vegetarian Food Available
+                </p>
+              </>
+            ) : record && record.category === "nonveg" ? (
+              <>
+                <FaRegDotCircle
+                  color="#ff0000"
+                  size={15}
+                  className="mr-2"
+                />
+                <p className="text-sm text-gray-600">
+                  Non Vegetarian Food Available
+                </p>
+              </>
+            ) : record && record.category === "both" ? (
+              <p className="text-sm text-gray-600">
+                Both veg and non-veg Food Available
+              </p>
+            ) : null}
           </div>
-          <div className='w-full md:w-6/12 '>
-            <div class="flex flex-nowrap">
-              <div>
-                <h1>{record.restaurantname}</h1>
-                <p>{record.category}</p>
-                <p>{record.description}</p>
-                <p>{record.location}</p>
-                <p>Timing :- {record.opening_to} {record.opening_from} </p>
-              </div>
-            </div>
-          </div>
+
+          
+          <p className="text-sm text-gray-700 mb-4 ">
+            {record && record.description}
+          </p>
         </div>
       </div>
-      <UserProduct userId ={userId}/>
+      <div className="w-full md:w-1/3 md:pt-4 pt-3">
+      <div className="flex flex-col justify-center h-full ">
+      <div className="flex items-center mb-4 mr-4">
+            <p className="text-sm text-gray-600 flex">
+              <FaLocationCrosshairs size={22} className="mr-2" />{" "}
+              {record && record.location}
+            </p>
+          </div>
+          <div className="flex items-center mb-4">
+            <p className="text-sm text-gray-600 flex ">
+              <FaRegClock size={15} className="mr-2 mt-0.5" /> Timing:{" "}
+              {record && record.opening_from} -{" "}
+              {record && record.opening_to}
+            </p>
+          </div>
+          </div>
+      </div>
+    </div>
+</div>
+
+      )}
     </>
-  )
+  );
 }
