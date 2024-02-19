47d927c8200dc996eb7b72dc9af7039f3ef030d2
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2024-02-19 11:17:14 +0530

    pwa app added

---
+++ public/service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'food-truck-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/index.css',
+];
+
+// Install event: Cache the specified URLs
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch event: Intercept network requests and serve cached responses
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+// Activate event: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((name) => {
+            if (name !== CACHE_NAME) {
+              return caches.delete(name);
+            }
+          })
+        );
+      })
+  );
+});
