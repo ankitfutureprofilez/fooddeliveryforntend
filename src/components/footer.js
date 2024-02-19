844575a62c3d5f46249875ec7da91c3272f4460b
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-02-19 18:22:21 +0530

    add

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-
+import instra from "../assest/instagram.png"
+import twitter from "../assest/twitter.png"
+import facebook from "../assest/facebook.png"
 
 const footer = () => {
   return (
@@ -8,13 +10,13 @@ const footer = () => {
             <a href="/"><img src="/footer-logo.png" class="h-full" /></a>
             <div className="flex items-center">
                 <a href="/">
-                    <img className="h-6 w-6" src="../assest/instagram.png" alt="" />
+                    <img className="h-6 w-6" src={instra} alt="" />
                 </a>
                 <a className="my-1.5" href="/">
-                    <img className="h-6 w-6" src="../assest/twitter.png" alt="" />
+                    <img className="h-6 w-6" src={twitter} alt="" />
                 </a>
                 <a href="/">
-                    <img className="h-6 w-6" src="../assest/facebook.png" alt="" />
+                    <img className="h-6 w-6" src={facebook} alt="" />
                 </a>
             </div>
         </div>
