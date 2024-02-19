a1ca0a244b3a0a73af4b1d6eb0584255777b3a7f
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-02-19 18:32:59 +0530

    aa

--- src/payment/Orderhistory.js
+++ src/payment/Orderhistory.js
@@ -107,6 +107,7 @@ export default function Orderhistory() {
                       <Link to={`/order_history/${item.order_id}`}>
                       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Track </button>
                       </Link>
+                      
                     </td>
                   </tr>
                 ))
