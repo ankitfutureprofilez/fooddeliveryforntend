35592708f46dcd67c9ce9457e422f0b5195bd6b0
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-02-20 16:55:09 +0530

    aa

--- src/components/HomeSlider.js
+++ src/components/HomeSlider.js
@@ -1,7 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import Product from './Product';
+import Listings from '../Api/Listings';
+import { formatMultiPrice } from '../hooks/Valuedata';
 
 export default function HomeSlider() {
+
+    const [Loading, setLoading] = useState(true);
+    useEffect(()=>{
+        const main = new Listings();
+        const response =  main.newproduct();
+        response.then((res)=>{
+            console.log("res",res)
+          const newData = res?.data;
+          
+          setLoading(false)
+        }).catch((error)=>{
+          console.log("error",error)
+          setLoading(false)
+        });
+    })
     return (
        <>
 
@@ -9,7 +26,33 @@ export default function HomeSlider() {
             <div className="container mx-auto">
                 <h2>Newly Added</h2>
                 <div className="flex">
-                   <Product/>
+                <div className="w-full bg-white product_box py-3 px-3 cursor-pointer flex flex-col rounded-xl">
+            
+              {/* <>
+                <div className="flex flex-col justify-center items-center">
+                  <img alt="image" src={image} className="rounded-xl w-full h-44 object-cover" />
+                </div>
+                <div>
+                  <h3 className="font-bold text-lg text-gray-900 capitalize text-base mt-3 mb-1 whitespace-nowrap overflow-hidden">
+                    {name}
+                  </h3>
+                  <div className="flex justify-between mt-3">
+                    <div>
+                      <p className="text-orange-500 text-sm font-bold align-middle">
+                        <span>{formatMultiPrice(price)}</span>
+                      </p>
+                      <p className="text-green-500">Free Delivery</p>
+                    </div>
+                    <button
+                      className="button bg-blue sm"
+                      onClick={handleAddCartProduct}
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+              </> */}
+          </div>
                 </div>
             </div>
         </div>
