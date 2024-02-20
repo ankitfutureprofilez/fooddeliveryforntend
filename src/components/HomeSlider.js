a3cf4058820759e201457cffc2821d1ab8d3aeb2
Abhinav-FP <a.mathur@futureprofilez.com>
2024-02-20 17:38:25 +0530

    HomeSlider data mapped

--- src/components/HomeSlider.js
+++ src/components/HomeSlider.js
@@ -1,33 +1,64 @@
-import React, { useEffect, useState } from 'react';
-import Product from './Product';
-import Listings from '../Api/Listings';
-import { formatMultiPrice } from '../hooks/Valuedata';
+import React, { useEffect, useState } from "react";
+import Product from "./Product";
+import Listings from "../Api/Listings";
+import { formatMultiPrice } from "../hooks/Valuedata";
 
 export default function HomeSlider() {
+  const [newData, setNewData] = useState([]);
+  const [Loading, setLoading] = useState(true);
+  useEffect(() => {
+    const main = new Listings();
+    const response = main.newproduct();
+    response
+      .then((res) => {
+        console.log("res", res.data.data);
+        setNewData(res?.data.data);
 
-    const [Loading, setLoading] = useState(true);
-    useEffect(()=>{
-        const main = new Listings();
-        const response =  main.newproduct();
-        response.then((res)=>{
-            console.log("res",res)
-          const newData = res?.data;
-          
-          setLoading(false)
-        }).catch((error)=>{
-          console.log("error",error)
-          setLoading(false)
-        });
-    })
-    return (
-       <>
-
-<div className='newly-added-sec'>
-            <div className="container mx-auto">
-                <h2>Newly Added</h2>
-                <div className="flex">
-                <div className="w-full bg-white product_box py-3 px-3 cursor-pointer flex flex-col rounded-xl">
-            
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("error", error);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <>
+      <div className="newly-added-sec">
+        <div className="container mx-auto">
+          <h2>Newly Added</h2>
+          <div className="flex">
+            <div className="w-full bg-white product_box py-3 px-3 cursor-pointer flex flex-col rounded-xl">
+              {newData &&
+                newData.map((item) => (
+                  <>
+                    <div className="flex flex-col justify-center items-center">
+                      <img
+                        alt="image"
+                        src={item.image}
+                        className="rounded-xl w-full h-44 object-cover"
+                      />
+                    </div>
+                    <div>
+                      <h3 className="font-bold text-lg text-gray-900 capitalize text-base mt-3 mb-1 whitespace-nowrap overflow-hidden">
+                        {item.name}
+                      </h3>
+                      <div className="flex justify-between mt-3">
+                        <div>
+                          <p className="text-orange-500 text-sm font-bold align-middle">
+                            <span>{formatMultiPrice(item.price)}</span>
+                          </p>
+                          <p className="text-green-500">Free Delivery</p>
+                        </div>
+                        <button
+                          className="button bg-blue sm"
+                          // onClick={handleAddCartProduct}
+                        >
+                          +
+                        </button>
+                      </div>
+                    </div>
+                  </>
+                ))}
               {/* <>
                 <div className="flex flex-col justify-center items-center">
                   <img alt="image" src={image} className="rounded-xl w-full h-44 object-cover" />
@@ -52,10 +83,10 @@ export default function HomeSlider() {
                   </div>
                 </div>
               </> */}
-          </div>
-                </div>
             </div>
+          </div>
         </div>
-       </>
-    );
+      </div>
+    </>
+  );
 }
