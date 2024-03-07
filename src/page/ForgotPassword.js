9e5c20d72e181c1cfd8532541e89300f335f268c
Abhinav-FP <a.mathur@futureprofilez.com>
2024-03-07 11:23:04 +0530

    Forgot Password Responsive

--- src/page/ForgotPassword.js
+++ src/page/ForgotPassword.js
@@ -46,52 +46,48 @@ export default function ForgotPassword() {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="bg-white rounded-xl shadow-lg md:w-1/2">
-        <div className="p-4 sm:p-7">
-          <div className="text-center">
-            <h1 className="text-3xl font-bold mb-6">Forgot password?</h1>
-            <p className="mt-2 text-sm text-gray-600">
-              Remember your password?
-              <Link to={"/login"} className="text-blue-500 ml-1">
-                Login here
-              </Link>
-            </p>
-          </div>
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
+      <div className="w-full max-w-sm m-auto flex flex-col p-4">
+        <h1 className="text-3xl font-bold mb-6 flex justify-center">Forgot password?</h1>
+        <p className="text-sm text-gray-600 mt-2 text-center">
+          Remember your password?
+          <Link to={"/login"} className="text-blue-500 ml-1 underline">
+            Login here
+          </Link>
+        </p>
   
-          <div className="mt-8">
-            <form onSubmit={handleSubmit}>
-              <div className="grid gap-y-4">
-                <label
-                  htmlFor="email"
-                  className="block uppercase tracking-wide text-gray-700 text-s font-bold"
-                >
-                  Email address
-                </label>
-                <div className="relative">
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    className="mb-2 w-full bg-gray-200 px-2 py-1 rounded focus-within:outline-blue-300"
-                    required
-                    aria-describedby="email-error"
-                    value={email}
-                    onChange={handleOnChange}
-                  />
-                  <p className="hidden text-xs text-red-600 mt-2" id="email-error">
-                    Please include a valid email address so we can get back to you
-                  </p>
-                </div>
-                <button
-                  type="submit"
-                  className="w-full bg-blue-600 hover:bg-blue-500 cursor-pointer text-white text-xl font-medium text-center py-2 rounded-full mt-4"
-                >
-                  {loading==false?`Generate OTP`:`Generating...`}
-                </button>
+        <div className="mt-8">
+          <form onSubmit={handleSubmit}>
+            <div className="grid gap-y-4">
+              <label
+                htmlFor="email"
+                className="block uppercase tracking-wide text-gray-700 text-s font-bold"
+              >
+                Email address
+              </label>
+              <div className="relative">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="mb-2 w-full bg-gray-200 px-2 py-1 rounded focus-within:outline-blue-300"
+                  required
+                  aria-describedby="email-error"
+                  value={email}
+                  onChange={handleOnChange}
+                />
+                <p className="hidden text-xs text-red-600 mt-2" id="email-error">
+                  Please include a valid email address so we can get back to you
+                </p>
               </div>
-            </form>
-          </div>
+              <button
+                type="submit"
+                className="w-full bg-blue-600 hover:bg-blue-500 cursor-pointer text-white text-xl font-medium text-center py-2 rounded-full mt-4"
+              >
+                {loading ? `Generating...` : `Generate OTP`}
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
